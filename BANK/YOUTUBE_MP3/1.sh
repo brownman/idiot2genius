0c1acb5a1a81583d71c5204027a954746d2dd769
brownman <brownman556@gmail.com>
2015-11-07 23:14:09 +0200

    try fix: file not found

--- BANK/YOUTUBE_MP3/1.sh
+++ BANK/YOUTUBE_MP3/1.sh
@@ -2,4 +2,5 @@ sudo apt-get install mailutils
 sudo apt-get install sendmail
 file=$(mktemp)
 echo hi > $file
-cat $file  | mail -s "My subject " $someone1@gmail.com
+maol=$( which mail )
+cat $file  | $mail -s "My subject " $someone1@gmail.com
