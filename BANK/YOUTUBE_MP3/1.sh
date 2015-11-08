228bc22ff9d358172a5b5529a4b6067893a2dcbe
brownman <brownman556@gmail.com>
2015-11-09 01:25:04 +0200

    Update 1.sh

--- BANK/YOUTUBE_MP3/1.sh
+++ BANK/YOUTUBE_MP3/1.sh
@@ -3,4 +3,6 @@ sudo apt-get install sendmail
 file=$(mktemp)
 echo hi > $file
 mail=$( which mail )
+echo "Subject: test" | /usr/lib/sendmail -v $someone1@gmail.com Example
+
 eval $( echo "cat $file  | $mail -s 'My subject'  $someone1@gmail.com")
