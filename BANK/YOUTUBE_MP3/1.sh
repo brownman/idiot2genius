b837333831d00e21fdb88ea28138aebcff46f5ef
brownman <brownman556@gmail.com>
2015-11-09 01:16:09 +0200

    typo

--- BANK/YOUTUBE_MP3/1.sh
+++ BANK/YOUTUBE_MP3/1.sh
@@ -2,5 +2,5 @@ sudo apt-get install mailutils
 sudo apt-get install sendmail
 file=$(mktemp)
 echo hi > $file
-maol=$( which mail )
+mail=$( which mail )
 eval $( echo "cat $file  | $mail -s 'My subject'  $someone1@gmail.com")
