8e4a7796947fa581664164b99b107e558b5311a3
brownman <brownman556@gmail.com>
2015-11-09 23:36:14 +0200

    Update 1.sh

--- BANK/YOUTUBE_MP3/1.sh
+++ BANK/YOUTUBE_MP3/1.sh
@@ -5,4 +5,4 @@ echo hi > $file
 mail=$( which mail )
 echo "Subject: test" | /usr/lib/sendmail -v $someone1@gmail.com Example
 
-eval $( echo "cat $file  | $mail -s 'My subject'  $someone1@gmail.com")
+ cat $file  | $mail -s 'My subject'  $someone1@gmail.com 
